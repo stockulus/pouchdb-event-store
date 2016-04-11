409c7e68423b3ebcb89e533e7bc5ca1106233cfe
stockulus <stockulus@icloud.com>
2016-04-11 16:28:31 +0200

    init files

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 'use strict'
 
-module.exports = function eventStoreFactory () {
+/**
+ * Factory function for the event store
+ * @param {object} options
+ * @param {object} options.pouchdb - pouchdb the events got stored
+ * @param {object} options.idGenerator - id generator for new resources
+ * @param {object[]} options.viewModels - Array of viewModels {pouchdb, reducer}
+ */
+module.exports = function eventStoreFactory (options) {
   return null
 }
