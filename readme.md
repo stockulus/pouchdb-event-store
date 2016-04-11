235ed6f3a33476c2abbbb8c3093c85c0b68a00ea
stockulus <stockulus@icloud.com>
2016-04-11 18:22:03 +0200

    push it

--- readme.md
+++ readme.md
@@ -8,4 +8,43 @@ npm i pouchdb-event-store
 ## test
 npm test
 
+```js
+const pouchdb = require('pouchdb')
+const eventStoreFactory = require('pouchdb-event-store')
+
+let lastId = 0
+const options = {
+  pouchdb: pouchdb('events'),
+  idGenerator: {
+    next () { return new Promise((resolve, reject) => { resolve(lastId++) }) }
+  },
+  viewModels: [
+    {
+      pouchdb: pouchdb('viewModel'),
+      reducer (state, event) {
+        if (!state.items) state.items = []
+        state.items.push(event)
+        return state
+      }
+    }
+  ]
+}
+
+eventStoreFactory(options)
+  .create()
+  .then((eventStore) => eventStore.add({type: 'CREATE'}, 123))
+  .then((result) => {
+    console.log(result)
+  })
+  .catch((error) => console.error(error))
+
+eventStoreFactory(options)
+  .get('1')
+  .then((eventStore) => {
+    console.log(eventStore.getEvents())
+  })
+  .catch((error) => console.error(error))
+
+```
+
 [![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg?style=flat)](http://standardjs.com/)
--- tests/index.js
+++ tests/index.js
@@ -3,7 +3,6 @@
 const test = require('tape')
 const fs = require('fs')
 const del = require('del')
-const idGeneratorFactory = require('seq-id-generator')
 const pouchdb = require('pouchdb')
   .defaults({prefix: '/tmp/eventStore/'})
 
@@ -12,29 +11,49 @@ const eventStoreFactory = require('../index')
 del.sync(['/tmp/eventStore'], {force: true})
 fs.mkdirSync('/tmp/eventStore')
 
-test('create new', (assert) => {
-  idGeneratorFactory('/tmp/eventStore/id.txt')
-    .then((idGenerator) => {
-      const options = {
-        pouchdb: pouchdb('events'),
-        idGenerator: idGenerator,
-        viewModels: [
-          {
-            pouchdb: pouchdb('viewModel'),
-            reducer (state, event) {
-              if (!state.items) state.items = []
-              state.items.push(event)
-              return state
-            }
-          }
-        ]
+const viewModelDb = pouchdb('viewModel')
+const options = {
+  pouchdb: pouchdb('events'),
+  idGenerator: {
+    next () { return new Promise((resolve, reject) => { resolve(1) }) }
+  },
+  viewModels: [
+    {
+      pouchdb: viewModelDb,
+      reducer (state, event) {
+        if (!state.items) state.items = []
+        state.items.push(event)
+        return state
       }
+    }
+  ]
+}
 
-      return eventStoreFactory(options).create()
-    })
+test('create new', (assert) => {
+  eventStoreFactory(options).create()
     .then((eventStore) => eventStore.add({type: 'CREATE'}, 123))
     .then((result) => {
       assert.equal(result.id, '1')
+      assert.end()
+    })
+    .catch((error) => assert.error(error))
+})
+
+test('read eventstore', (assert) => {
+  eventStoreFactory(options).get('1')
+    .then((eventStore) => {
+      assert.equal(eventStore.getId(), '1')
+      assert.equal(eventStore.getEvents().length, 1)
+      assert.end()
+    })
+    .catch((error) => assert.error(error))
+})
+
+test('read viewModel', (assert) => {
+  viewModelDb.get('1')
+    .then((viewModel) => {
+      assert.equal(viewModel._id, '1')
+      assert.equal(viewModel.items.length, 1)
       assert.end()
     })
     .catch((error) => assert.error(error))
